90a12fcc44de18c1255841cf2a29419bd227aa67
mkartha <madhav.kartha2019@gmail.com>
2025-11-16 23:18:23 -0600

    Refactor MonteCarlo class constructor and methods
    Fix constructor and return calculation in MonteCarlo class

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -2,12 +2,25 @@ import yfinance as yf
 import pandas as pd
 import matplotlib as mp
 from datetime import date
+import numpy as np
 
 class MonteCarlo:
 
   #Constructor
-  def __init__(ticker, st):
+  def __init__(self,ticker, st):
     self.stock_data=yf.download(ticker, start=st, end=date.today(), interval="1mo", progress=False)
-    self.returns=[]
-    for 
+    self.returns=None
+
+  def calculate_returns(self):
+    data_returns=[]
+    for i in range(self.stock_data):
+      if(i<len(self.stock_data["Open"])-1):
+        tStock=self.stock_data["Open"].iloc[i]
+        nStock=self.stock_data["Open"].iloc[i+1]
+        data_returns.append(np.log(nStock/tStock))
+        i+=1
+        self.returns=data_returns;
+    return self.returns
+  
+      
     
