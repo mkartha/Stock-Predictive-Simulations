fc82e9221398461282c9b667a7d126b3578926fc
mkartha <madhav.kartha2019@gmail.com>
2025-11-22 23:52:34 -0600

    Clarify input prompt for market predictability rating

--- TestMarket.py
+++ TestMarket.py
@@ -5,7 +5,7 @@ class TestMarket:
     sTech=int(input("If the corporation you are trying to analyze is tech, rate the current growth of the tech market"))
     sEco=int(input("How stressed would you say the current U.S. Economy is due to various factors (e.g. Recession, Inflation)"))
     sMT=int(input("On a scale of 1-5(1 being not much increasing trend, and 5 being clear increasing), how increasing would you say the current market is?"))
-    sDR=int(input("What is the predictability of the market (1-highest degree, low predictability)"))
+    sDR=int(input("Rate the predictability of the market (1 = very unpredictable, 5 = very predictable)"))
     tScore=(0.4*(5-sEco))+(0.25*(sTech))+(0.25*(sMT))+(0.1*(sDR))
     if tScore>2.9:
       return "Bull"
