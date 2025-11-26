7114f1d2d90bdce7d6fbcc3ce5a074ce7559fa48
mkartha <madhav.kartha2019@gmail.com>
2025-11-25 23:52:37 -0600

    Enhance Monte Carlo simulation for stock predictions
    Refactor Monte Carlo simulation for stock price predictions, including data cleaning and improved calculations for returns, volatility, and drift.

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -1,6 +1,12 @@
+#Monte Carlo implementation for Stock Predictive Simulations
+#Best Utilized for Bear Markets
+#Completed on November 25 2025
+#© MK 2025, All Rights Reserved
+
+
 import yfinance as yf
 import pandas as pd
-import matplotlib as mp
+import matplotlib.pyplot as mp
 from datetime import date
 import numpy as np
 
@@ -8,43 +14,69 @@ class MonteCarlo:
 
   #Constructor
   def __init__(self,ticker, st):
-    self.stock_data=yf.download(ticker, start=st, end=date.today(), interval="1mo", progress=False)
+    raw=yf.download(ticker, start=st, end=date.today(), interval="1mo", progress=False)
+    df=pd.DataFrame(raw)
+    df_cleaned=df.dropna()
+    self.stock_data=df_cleaned
     self.returns=None
+    self.volatility=None
+    self.drift=None
 
   def calculate_returns(self):
     data_returns=[]
-    for i in range(self.stock_data):
-      if(i<len(self.stock_data["Open"])-1):
-        tStock=self.stock_data["Open"].iloc[i]
-        nStock=self.stock_data["Open"].iloc[i+1]
-        data_returns.append(np.log(nStock/tStock))
-        i+=1
-        self.returns=data_returns;
+    for i in range(len(self.stock_data["Open"]) - 1):
+      tStock=self.stock_data["Open"].iloc[i]
+      nStock=self.stock_data["Open"].iloc[i+1]
+      data_returns.append(np.log(nStock/tStock))
+    self.returns=data_returns
     return self.returns
 
   #Calculate stock volatility(Standard Deviation)
   def calc_volatility(self):
+    if self.returns is None:
+      self.calculate_returns()
     self.volatility=np.std(self.returns)
     return self.volatility
 
   #Calculate drift(Mean)
   def calc_drift(self):
+    if self.returns is None:
+      self.calculate_returns()
     self.drift=np.mean(self.returns)
     return self.drift
 
   #Monte carlo simulation using GBM
   def simulate_month_ahead(self, months_ahead, n_sims):
-    T=months_ahead/12.0
-    last_m=self.returns[-1]
+    if self.drift is None:
+      self.calc_drift()
+    
+    if self.volatility is None:
+      self.calc_volatility()
+    
+    T=months_ahead
     m_carlo=[]
+    closing_price=self.stock_data["Close"].iloc[-1]
     for i in range(n_sims):
       Z=np.random.normal()
-      future_log_return = ((self.drift - 0.5 * self.volatility**2) * T + self.volatility * np.sqrt(T) * Z)
-      m_carlo.append(tr)
-    expected_return=np.mean(m_carlo)
-    closing_price=self.stock_data["Adj Close"].iloc[-1]
-    p_value=closing_price+(closing_price*np.exp(expected_return))
-    return p_value
+      tr=((self.drift - 0.5 * self.volatility**2) * T + self.volatility * np.sqrt(T) * Z)
+      m_carlo.append(np.exp(tr))
+    e_p=closing_price*np.mean(m_carlo)
+    return e_p
+  
+  #Visualize n_month random simulations
+  def visualize_randoms(self, n_months):
+    x=np.arange(1,n_months+1)
+    y=[]
+    for i in range(1,n_months+1):
+      rand=self.simulate_month_ahead(i,1000)
+      y.append(rand)
+    mp.scatter(x,y,color='black', marker='o')
+    mp.plot(x,y,color='purple',linestyle='-')
+    mp.xlabel("Month")
+    mp.ylabel("Simulated Stock price")
+    mp.show()
+
+
       
   
     
